f34fcf09d6c78dcd4f4a8d64103bd3641c594db0
sakurai <sakurai@sakurai-mbp.local>
2017-02-01 16:08:51 +0900

    add person class and readme file

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,19 @@
 var Sample = require('./sample.js');
+var Person = require('./person.js');
 
 var sample = new Sample;
 sample.getNumber(); // -> Number is 1
 sample.setNumber(5);
 sample.getNumber(); // -> Number is 5
+
+var person = new Person(1, 'sakurai', 19840914)
+console.log(person.toString())
+console.log(person)
+console.log(person.name)
+console.log(person.MAX_ID)
+console.log(Person.author)
+console.log(person.author)
+console.log(Person.AUTO_ID)
+console.log(person.AUTO_ID)
+console.log(Person.ja())
+// console.log(person.ja())
---
+++ src/person.js
@@ -0,0 +1,24 @@
+class Person {
+  constructor(id = -1, name, birth) {
+    this.id = id
+    this.name = name
+    this.birth = birth
+  }
+  // stage を npm install したら使えるけど stage は使わないほうが良さそう
+  // toString = () => ["id: " + this.id, "name: " + this.name, "birth: " + this.birth].join(",")
+  toString() {
+    return ["id: " + this.id, "name: " + this.name, "birth: " + this.birth].join(",")
+  }
+
+  get MAX_ID() {
+    return 9999
+  }
+  get MAX_ID() {
+    return 9999
+  }
+  static get author() { return "sakurai@weblio.co.jp" }
+  static get AUTO_ID() { return 0 }
+  static ja() { return "japan" }
+}
+
+module.exports = Person;
