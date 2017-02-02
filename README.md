fc615717554ccca53c9570a4eef0628e9508be0b
sakurai <sakurai@sakurai-mbp.local>
2017-02-02 14:40:39 +0900

    node-sass and webpack-dev-server test add

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-#babelのテスト
+#TEST babel,node-sass and webpack-dev-server
 - privateなプロパティやメソッドは作成できない
+- webpack-dev-server はsave-devでnpm installする必要がある（？）
+- `npm start`で`http://localhost:3000`にて動作確認できる
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,33 @@
+var webpack = require("webpack");
 module.exports = {
+  context: __dirname + '/src',
   // ビルドの基点となるファイル
-  entry: './src/entry.js',
+  entry: {
+    javascript: './js/entry.js'
+  },
   // ビルド後のファイル
   output: {
-    path: __dirname + '/dist',
+    path: __dirname + '/src/dist',
     filename: 'bundle.js'
   },
+  devServer: {
+    contentBase: 'src',
+    port: 3000,
+    inline: true,
+    hot: true
+  },
+  devtool: '#source-map',
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ],
   // 拡張子が.jsのファイルはbabel-loaderを通してビルド(node_modulesは除外)
   module: {
-    loaders: [{
+    loaders: [
+      {
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
-    }]
+    }
+  ]
   }
 };
